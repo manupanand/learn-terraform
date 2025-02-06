f7c430781309c4bb32a703c60e34af4e986b415f
manupanand <manupanandindianinvestor@gmail.com>
2025-02-06 22:55:17 +0530

    update azure test code

--- revice/learn-code/01-resource-azr/vm.tf
+++ revice/learn-code/01-resource-azr/vm.tf
@@ -1,7 +1,43 @@
+
+variable "prefix" {
+  default = "devops"
+}
+
+resource "azurerm_resource_group" "devops-test" {
+  name     = "${var.prefix}-resources"
+  location = "Central India"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.devops-test.location
+  resource_group_name = azurerm_resource_group.devops-test.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.devops-test.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.devops-test.location
+  resource_group_name = azurerm_resource_group.devops-test.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+# Create the Virtual Machine
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.devops-test.location
+  resource_group_name   = azurerm_resource_group.devops-test.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
 
@@ -24,14 +60,14 @@ resource "azurerm_virtual_machine" "main" {
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
+    computer_name  = "devops"
+    admin_username = "ubuntu"
     admin_password = "Password1234!"
   }
   os_profile_linux_config {
     disable_password_authentication = false
   }
   tags = {
-    environment = "staging"
+    environment = "development"
   }
 }
