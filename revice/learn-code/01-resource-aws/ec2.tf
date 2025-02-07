c1febb93dc55cdb2ef08116e7b7dfcdcd2cd72d4
manupanand <manupanandindianinvestor@gmail.com>
2025-02-07 20:46:58 +0530

    update ssh passowrd test

--- revice/learn-code/01-resource-aws/ec2.tf
+++ revice/learn-code/01-resource-aws/ec2.tf
@@ -2,6 +2,18 @@ resource "aws_instance" "web_local"{
     instance_type="t3.micro"
     ami="ami-0d81f2cd09b410166"
     vpc_security_group_ids = ["sg-00ac6f603cfd19030"]
+
+    # Pass a shell script as user_data to set the password
+  user_data = <<-EOF
+              #!/bin/bash
+              # Set the password for the user "ec2-user" or any user you have
+              echo "ec2-user:password@TEST123" | chpasswd
+              # Optionally enable password authentication if it's disabled by default
+              sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+              systemctl restart sshd
+              EOF
+
+
     tags={
         Name="demo-terraform"
     }
