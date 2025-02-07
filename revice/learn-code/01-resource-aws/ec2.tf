c2c7daf3ba673dff04b99fd818af2633cced9c2b
manupanand <manupanandindianinvestor@gmail.com>
2025-02-07 17:06:40 +0530

    update code

--- revice/learn-code/01-resource-aws/ec2.tf
+++ revice/learn-code/01-resource-aws/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web_local"{
     instance_type="t3.micro"
     ami="ami-0d81f2cd09b410166"
+    vpc_security_group_ids = ["sg-00ac6f603cfd19030"]
     tags={
         Name="demo-terraform"
     }
