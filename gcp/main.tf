b4946238ba23cf6944b76b1cc696ef91e585a961
manupanand <manupanandindianinvestor@gmail.com>
2025-10-08 16:56:33 +0530

    update code

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project     = ""
-  region      = ""
+  project     = "zeta-flare-449207-r0"
+  region      = "asia-south1"
 }
 resource "google_compute_network" "vpc_network" {
-  project                 = ""
+  project                 = "zeta-flare-449207-r0"
   name                    = "vpc-network"
   auto_create_subnetworks = true
   mtu                     = 1460
